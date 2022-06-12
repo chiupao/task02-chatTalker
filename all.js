6841a5c47053868d1050dac0a1d1df6216137d3a
chiupao <a0912979737@gmail.com>
2022-06-12 22:45:25 +0800

    完成頁面二、加入 jQuery

--- all.js
+++ all.js
@@ -1,8 +1,80 @@
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
+$(document).ready(function () {
+  // ----- TOP -----
+  $(".scrollTopBtn").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        // 有些瀏覽器只支援html，有些只支援body 所以兩個都寫進去
+        scrollTop: 0,
+      },
+      700
+    );
+  });
 
+  // ----- program -----
+  $(".programNumBtn").click(function (e) {
+    e.preventDefault();
 
+    // 樣式切換
+    $(this).addClass("active");
+    $(this).parent().siblings().find(".programNumBtn").removeClass("active");
+
+    switch ($(this).text()) {
+      case "10000位":
+        $("#basicNum").html("10000<span>位</span>");
+        $("#standardNum").html("10000<span>位</span>");
+        $("#basicFee").html("600<span>TWD</span>");
+        $("#standardFee").html("1600<span>TWD</span>");
+        break;
+      case "15000位":
+        $("#basicNum").html("15000<span>位</span>");
+        $("#standardNum").html("15000<span>位</span>");
+        $("#basicFee").html("500<span>TWD</span>");
+        $("#standardFee").html("1500<span>TWD</span>");
+        break;
+      case "20000位":
+        $("#basicNum").html("20000<span>位</span>");
+        $("#standardNum").html("20000<span>位</span>");
+        $("#basicFee").html("400<span>TWD</span>");
+        $("#standardFee").html("1400<span>TWD</span>");
+        break;
+      case "25000位":
+        $("#basicNum").html("25000<span>位</span>");
+        $("#standardNum").html("25000<span>位</span>");
+        $("#basicFee").html("300<span>TWD</span>");
+        $("#standardFee").html("1300<span>TWD</span>");
+        break;
+      case ">25000位":
+        $("#basicNum").html(">25000<span>位</span>");
+        $("#standardNum").html(">25000<span>位</span>");
+        $("#basicFee").html("200<span>TWD</span>");
+        $("#standardFee").html("1200<span>TWD</span>");
+        break;
+    }
+  });
+
+  // ----- QA -----
+  $(".questionBlock").click(function () {
+    // 樣式切換
+    $(this).toggleClass("active");
+
+    // ＋ 或 - 圖片切換
+    $(this).find("img").attr("src") === "./images/icon_plus.svg"
+      ? $(this).find("img").attr("src", "./images/icon_minus.svg")
+      : $(this).find("img").attr("src", "./images/icon_plus.svg");
+
+    // 文字打開
+    $(this).siblings("p").slideToggle();
+
+    // 其餘文字收合
+    $(this).parent().siblings().find("p").slideUp();
+
+    // 其餘樣式取消
+    $(this).parent().siblings().find(".questionBlock").removeClass("active");
+  });
+});
+
+// ----- Swiper -----
 function initSwiper() {
   /* 
   id="comment-swiper" 區塊是使用 swiper 套件的範圍
